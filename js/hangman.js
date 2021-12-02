19531f5e5988b0dd1d9f4e19e298feeb1a87c528
Terrance <tmikebusiness@gmail.com>
2021-12-01 20:39:19 -0800

    updated word bank

--- js/hangman.js
+++ js/hangman.js
@@ -1,15 +1,36 @@
-const wordBank = ["terrance",'anette','babyd','booboo','blackstar','anthony','geneva','baldy','deszie','breanna','vanessa','nick','steffy','rayray','raymond','nini','rayla','ninj','woopwoop', 'emma'];
+const wordBank = [
+  "terrance",
+  "anette",
+  "babyd",
+  "booboo",
+  "blackstar",
+  "anthony",
+  "geneva",
+  "baldy",
+  "deszie",
+  "breanna",
+  "vanessa",
+  "nick",
+  "steffy",
+  "rayray",
+  "raymond",
+  "nini",
+  "rayla",
+  "ninj",
+  "woopwoop",
+  "emma",
+  "chanel",
+];
 const keyboard = document.querySelector(".keyboard");
 // const hiddenWord = document.querySelector(".wordSpotlight");
 const currentWord = [];
 const resetButton = document.querySelector(".resetbutton");
-const startButton = document.querySelector('.beginbutton');
+const startButton = document.querySelector(".beginbutton");
 let maxWrong = 10;
 let mistakes = 0;
 const guessedCorrect = [];
 let wordStatus = null;
 // Generate The Game on Load
-
 
 resetButton.addEventListener("click", resetGame);
 startButton.addEventListener("click", gamePlay);
@@ -17,15 +38,14 @@ startButton.addEventListener("click", gamePlay);
 generateButtons();
 
 function resetGame() {
-    let answerWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-    currentWord.push(answerWord);
-    console.log(mistakes);
-    console.log(currentWord);
-    console.log(guessedCorrect);
-    console.log("game reset");
-    console.log(answerWord);
+  let answerWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+  currentWord.push(answerWord);
+  console.log(mistakes);
+  console.log(currentWord);
+  console.log(guessedCorrect);
+  console.log("game reset");
+  console.log(answerWord);
 
-  
   let disableButton = document.querySelectorAll(".gamebuttons");
   for (let k = 0; k < disableButton.length; k++) {
     disableButton[k].classList.remove(
@@ -36,22 +56,22 @@ function resetGame() {
   }
   document.querySelector(".mistakes").innerHTML = 0;
   mistakes = 0;
-  
+
   document.querySelector(".winlose").innerHTML = "";
   hideWord(answerWord);
- 
 }
 
 function gamePlay() {
-    startButton.classList.add(
-        "opacity-25",
-        "pointer-events-none",
-        "duration-300");
-    console.log('gameStart')
+  startButton.classList.add(
+    "opacity-25",
+    "pointer-events-none",
+    "duration-300"
+  );
+  console.log("gameStart");
   let answerWord = wordBank[Math.floor(Math.random() * wordBank.length)];
   currentWord.push(answerWord);
   hideWord(answerWord);
-  
+
   mistakes = 0;
 }
 
@@ -70,8 +90,6 @@ ${final}
 </span>`;
 }
 
-
-
 function handleGuess(input) {
   // Disable the button
   let disableButton = document.querySelectorAll("button." + input);
@@ -81,28 +99,28 @@ function handleGuess(input) {
       "pointer-events-none",
       "duration-300"
     );
-
   }
 
   // Handle Correct Answer
-  let parseArray = currentWord[currentWord.length-1].split("");
+  let parseArray = currentWord[currentWord.length - 1].split("");
   if (parseArray.indexOf(input) >= 0) {
     for (let c = 0; c < parseArray.length; c++) {
       if (input == parseArray[c]) {
-          let targets = document.querySelectorAll('span.'+input);
-          
-       for(let r = 0; r<targets.length; r++){
-        document.querySelectorAll("span." + input)[r].classList.add('correct')
-        console.log(document.querySelectorAll("span." + input));
-       }
+        let targets = document.querySelectorAll("span." + input);
+
+        for (let r = 0; r < targets.length; r++) {
+          document
+            .querySelectorAll("span." + input)
+            [r].classList.add("correct");
+          console.log(document.querySelectorAll("span." + input));
+        }
       }
       let guessLetter = document.querySelectorAll("span." + input + "");
       for (let r = 0; r < guessLetter.length; r++) {
         guessLetter[r].innerHTML = input;
-        
       }
     }
-    console.log(document.querySelectorAll('.correct').length)
+    console.log(document.querySelectorAll(".correct").length);
   } else {
     mistakes++;
     document.querySelector(".mistakes").innerHTML = mistakes;
@@ -119,8 +137,11 @@ function handleGuess(input) {
         "duration-300"
       );
     }
-    document.querySelector('.graveyard').innerHTML = currentWord.join(', ')
-  } else if (document.querySelectorAll('.correct').length === currentWord[currentWord.length-1].length) {
+    document.querySelector(".graveyard").innerHTML = currentWord.join(", ");
+  } else if (
+    document.querySelectorAll(".correct").length ===
+    currentWord[currentWord.length - 1].length
+  ) {
     document.querySelector(".winlose").innerHTML = "YOU WIN";
     let gamebuttons = document.querySelectorAll(".gamebuttons");
     for (let m = 0; m < gamebuttons.length; m++) {
@@ -130,7 +151,7 @@ function handleGuess(input) {
         "duration-300"
       );
     }
-    document.querySelector('.graveyard').innerHTML = currentWord.join(', ')
+    document.querySelector(".graveyard").innerHTML = currentWord.join(", ");
   }
 }
 function generateButtons() {
